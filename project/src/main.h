8453f9de6a121040b616e11d393d87da85ea516b
Juan Mercurio <juanmercuriobadii@gmail.com>
2021-08-18 11:17:46 -0300

    Cambio como se incluye la shared

--- project/src/main.h
+++ project/src/main.h
@@ -1,11 +1,9 @@
 #ifndef _MAIN_1_
 #define _MAIN_1_
 
-#include <stdlib.h>
-#include <stdio.h>
 #include "tests/tests.h"
 
-#include "utils/utils.h" 
-#include "conexiones/conexiones.h"
+#include <utils/utils.h> 
+#include <conexiones/conexiones.h>
 
 #endif
