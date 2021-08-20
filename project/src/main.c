a7d990520d2268a815f53a15b73a30e6446966e5
Juan Mercurio <juanmercuriobadii@gmail.com>
2021-08-20 16:49:32 -0300

    Cambio tareas a nombres mas intuitivos:
     - Problema: Cuando se usa la tarea "Build All" solo aparecen
                 los warnings en las utils

--- project/src/main.c
+++ project/src/main.c
@@ -4,9 +4,11 @@
 int main(int argc, char* argv[]) {
    
    correr_tests(argc, argv[1]);    //solo corre si "argv[1]" es "test" (si el argumento que le pasas al binario en "test")
-  
-  while(1){
-     helloWorld();
-     sleep(2);
-  }
+   
+   while(1){
+      helloWorld();
+      sleep(2);
+   }
+   
+   return 0;
 }
